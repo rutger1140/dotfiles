886e036fff6e4b8a7cb5b29b3d3aa17c3adf53f2
Rutger <2-rutger@users.noreply.codepot.nl>
2025-01-08 16:06:25 +0100

    Update NVIM settings
    - Updated plugins with lazy
    - Corrected background and theme for catppuccin
    - Added indent plugin from folke
    - Enabled icons in lualine
    - Switched the neotree to toggle with reveal

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,8 +1,11 @@
-return  {
+return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme "catppuccin"
+    require("catppuccin").setup({
+      transparent_background = true,
+    })
+    vim.cmd.colorscheme "catppuccin-mocha"
   end
 }
--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -7,7 +7,7 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
+		vim.keymap.set("n", "<C-n>", ":Neotree toggle reveal reveal_force_cwd<CR>", {})
 	end,
 	opts = {
 		filesystem = {
