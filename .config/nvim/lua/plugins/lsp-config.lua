baba34797e112d387e256aad9f13b2d597c7c926
Rutger <2-rutger@users.noreply.codepot.nl>
2025-03-18 10:48:18 +0100

    Update Lazy packages + restructure lspconfig
    Added cssls

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,42 +1,35 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"tailwindcss",
-					"intelephense",
-					"ruby_lsp",
-					"dockerls",
-					"docker_compose_language_service",
-					"html",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.tailwindcss.setup({})
-			lspconfig.intelephense.setup({})
-			lspconfig.ruby_lsp.setup({})
-			lspconfig.dockerls.setup({})
-			lspconfig.docker_compose_language_service.setup({})
-			lspconfig.html.setup({})
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.tailwindcss.setup({})
+      lspconfig.intelephense.setup({})
+      lspconfig.ruby_lsp.setup({})
+      lspconfig.dockerls.setup({})
+      lspconfig.docker_compose_language_service.setup({})
+      lspconfig.html.setup({})
+      lspconfig.cssls.setup({})
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP hover" })
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "LSP definition" })
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "LSP references" })
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP code action" })
-		end,
-	},
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP hover" })
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "LSP definition" })
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "LSP references" })
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP code action" })
+    end,
+  },
 }
